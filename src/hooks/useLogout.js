5ebbcea1786cf5807b543c1d99727a05845089bd
SatkarNiraula <satkarniraula69@gmail.com>
2024-09-05 10:35:13 +0545

    update :: some minor refactoring

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -11,23 +11,10 @@ const useLogout = () => {
   const router = useRouter();
   const logout = async () => {
     setLoading(true);
-    try {
-      const res = await fetch("http://localhost:5000/api/auth/logout", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await res.json();
-      if (data.error) {
-        throw new Error(data.error);
-      }
-      Cookies.remove("chat-user");
-      setAuthUser(null);
-      router.refresh();
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
+    Cookies.remove("chat-user");
+    setAuthUser(null);
+    setLoading(false);
+    router.refresh();
   };
   return { loading, logout };
 };
