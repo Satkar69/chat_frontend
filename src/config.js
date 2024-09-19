ca5f3f9011ad70c596db307ac7efa7e758b976de
SatkarNiraula <satkarniraula69@gmail.com>
2024-09-19 17:29:35 +0545

    update :: socket api end point updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // process.env.API_LOCAL_URL for local
 
-const apiUrl = "https://chat-backend-blue-tau.vercel.app/api";
+// const apiUrl = "https://chat-backend-blue-tau.vercel.app/api";
+const apiUrl = process.env.API_LOCAL_URL || "http://localhost:5000/api";
 
 export const endpoints = {
   login: `${apiUrl}/auth/login`,
