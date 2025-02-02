fa4d643630a6045296bc5f992648d71763613bd6
SatkarNiraula <satkarniraula69@gmail.com>
2025-02-02 18:24:29 +0545

    update :: insert production backend api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const apiUrl = "http://localhost:5000/api";
+// const localApiUrl = "http://localhost:5000/api";
+const prodApiUrl = "https://chat-backend-241r.onrender.com/api";
+const apiUrl = prodApiUrl;
 
 export const endpoints = {
   login: `${apiUrl}/auth/login`,
